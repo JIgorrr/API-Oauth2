f4a2aa88d83bdee088fcc1458b57afff18340915
igordev <joaoigor456@gmail.com>
2025-03-30 23:35:24 -0300

    Push do projeto no github

---
+++ Validate/ModelClientCredentialsValidate.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using GenerateAccessToken.Model;
+
+namespace GenerateAccessToken.Validate;
+
+public class ModelClientCredentialsValidate : AbstractValidator<ModelClientCredentials>
+{
+    public ModelClientCredentialsValidate()
+    {
+        RuleFor(x => x.ClientId)
+            .NotNull()
+            .WithMessage("Campo obrigatório");
+
+        RuleFor(x => x.ClientSecret)
+            .NotNull()
+            .WithMessage("Campo obrigatório");
+
+        RuleFor(x => x.GrantType)
+            .NotNull()
+            .WithMessage("Campo obrigatório");
+
+        RuleFor(x => x.Scopes)
+            .NotNull()
+            .WithMessage("Campo obrigatório");
+    }
+}
